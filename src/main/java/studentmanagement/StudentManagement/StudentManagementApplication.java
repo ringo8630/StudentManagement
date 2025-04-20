261e7c8a1c0805b3aec81864441651f54b74dee6
ringo <sakura.h8687@gmail.com>
2025-04-20 14:30:44 +0900

    kadai7torikumicyuudesu.

--- src/main/java/studentmanagement/StudentManagement/StudentManagementApplication.java
+++ src/main/java/studentmanagement/StudentManagement/StudentManagementApplication.java
@@ -1,14 +1,12 @@
 package studentmanagement.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,34 +20,39 @@ public class StudentManagementApplication {
 	private StudentRepository repository;
 
 
-	private  String name = "Enami Kouji";
-	private  String age = "37";
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/studentInfo")
-	public String getstudentInfo() {
-		Students student = repository.searchByName("EnamiKouji");
-		if (student == null) {
-			return "Student not found";
-		}
+	@GetMapping("/student")
+	public String getstudent(@RequestParam("name")String name) {
+		Student student = repository.searchByName(name);
+		//if (student == null) {
+		//	return "Student not found";
+		//}
 		return student.getName() + " " + student.getAge() + "歳";
 	}
 
 
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name,String age){
-		this.name = name;
-		this.age = age;
+	@PostMapping("/student")
+	public void registerStudent(String name, int age){
+		repository.registerStudent(name, age);
+
 
 	}
-		@PostMapping("/studentName")
-		public void updateStudentName(String name) {
-			this.name = name;
+		@PatchMapping("/student")
+		public void updateStudentName(String name,int age) {
+			repository.updateStudent(name, age);
 		}
-}
+
+	@DeleteMapping("/student")
+			public void deleteStudent(String name) {
+		repository.deleteStudent(name);
+			}
+
+		}
+
 
 
--- src/main/java/studentmanagement/StudentManagement/StudentRepository.java
+++ src/main/java/studentmanagement/StudentManagement/StudentRepository.java
@@ -1,11 +1,24 @@
 package studentmanagement.StudentManagement;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM students WHERE name = #{name}")
-  Students searchByName(String name);
+  @Select("SELECT name,age FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+
+  @Insert("INSERT INTO student(name, age) values (#{name},#{age})")
+  void registerStudent(String name, int age);
+
+  @Update("UPDATE student SET age = #{age} WHERE name =#{name}")
+  void updateStudent(String name,int age);
+
+  @Delete("DELETE FROM student WHERE name = #{name}")
+  void deleteStudent(String name);
 }
